63e4b95e893bf50f470b1b1091ef707c896440ca
Sergei Chipiga <Sergei.Chipiga@derivco.ee>
2017-12-13 09:23:34 +0200

    update glace version

--- tests/unit/testSteps.js
+++ tests/unit/testSteps.js
@@ -1,23 +1,16 @@
 "use strict";
 
-var chai = require("chai");
-var chai_as_promised = require("chai-as-promised");
-var sinon = require("sinon");
-
 var CONF = require("../../lib/config");
 var Steps = require("../../lib/steps");
-
-chai.use(chai_as_promised);
-var expect = chai.expect;
 
 scope("Steps", () => {
     var ctx;
 
-    test(".setResolution()", () => {
+    test(".setViewport()", () => {
 
         before(() => {
             ctx = {};
-            ctx.setResolution = Steps.setResolution;
+            ctx.setViewport = Steps.setViewport;
         });
 
         beforeChunk(() => {
@@ -30,7 +23,7 @@ scope("Steps", () => {
         chunk("sets browser viewport size from options", async () => {
             var size = { width: 100, height: 100 };
             ctx.webdriver.getViewportSize.returns(size);
-            expect(await ctx.setResolution(size)).to.be.true;
+            expect(await ctx.setViewport(size)).to.be.true;
             expect(ctx.webdriver.setViewportSize.calledOnce).to.be.true;
             expect(ctx.webdriver.getViewportSize.calledOnce).to.be.true;
         });
@@ -40,13 +33,13 @@ scope("Steps", () => {
             CONF.web.width = size.width;
             CONF.web.height = size.height;
             ctx.webdriver.getViewportSize.returns(size);
-            expect(await ctx.setResolution()).to.be.true;
+            expect(await ctx.setViewport()).to.be.true;
             expect(ctx.webdriver.setViewportSize.calledOnce).to.be.true;
             expect(ctx.webdriver.getViewportSize.calledOnce).to.be.true;
         });
 
         chunk("doesn't check viewport size if check is disabled", async () => {
-            expect(await ctx.setResolution({ width: 100,
+            expect(await ctx.setViewport({ width: 100,
                                              height: 100,
                                              check: false })).to.be.true;
             expect(ctx.webdriver.setViewportSize.calledOnce).to.be.true;
@@ -55,23 +48,23 @@ scope("Steps", () => {
 
         chunk("throws error if viewport height wasn't updated", async () => {
             ctx.webdriver.getViewportSize.returns({ width: 100 });
-            await expect(ctx.setResolution({ width: 100, height: 100 }))
+            await expect(ctx.setViewport({ width: 100, height: 100 }))
                 .to.be.rejectedWith("height");
         });
 
         chunk("throws error if viewport width wasn't updated", async () => {
             ctx.webdriver.getViewportSize.returns({ height: 100 });
-            await expect(ctx.setResolution({ width: 100, height: 100 }))
+            await expect(ctx.setViewport({ width: 100, height: 100 }))
                 .to.be.rejectedWith("width");
         });
 
         chunk("throws error if passed invalid height value", async () => {
-            await expect(ctx.setResolution({ width: 100, height: null }))
+            await expect(ctx.setViewport({ width: 100, height: null }))
                 .to.be.rejectedWith("height");
         });
 
         chunk("throws error if passed invalid width value", async () => {
-            await expect(ctx.setResolution({ width: null, height: 100 }))
+            await expect(ctx.setViewport({ width: null, height: 100 }))
                 .to.be.rejectedWith("width");
         });
     });
