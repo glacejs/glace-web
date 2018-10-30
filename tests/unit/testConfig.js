e200bd58fa685461564715e9d82befbf4ccca69d
Sergei Chipiga <sergei.chipiga@pipedrive.com>
2018-10-30 08:48:54 +0200

    Install missed drivers #44

--- tests/unit/testConfig.js
+++ tests/unit/testConfig.js
@@ -9,32 +9,5 @@ suite("config", () => {
         chunk("contains web section", () => {
             expect(CONF.web).to.exist;
         });
-    
-        scope("has installDrivers", () => {
-    
-            var reloadConfig = () => {
-                delete CONF.args.updateWebDrivers;
-                delete require.cache[require.resolve("../../lib/config")];
-            };
-    
-            beforeChunk(() => {
-                reloadConfig();
-            });
-    
-            afterChunk(() => {
-                reloadConfig();
-                CONF = require("../../lib/config");
-            });
-    
-            chunk("true by default", () => {
-                expect(CONF.web.updateDrivers).to.be.false;
-            });
-    
-            chunk("false if CLI option set", () => {
-                CONF.args.updateWebDrivers = true;
-                CONF = require("../../lib/config");
-                expect(CONF.web.updateDrivers).to.be.true;
-            });
-        });
     });
 });
